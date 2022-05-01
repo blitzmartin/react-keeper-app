67388ef1939d850d1603d074633d6075509d37ca
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-05-01 14:09:16 +0200

    array map for the notes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Note'
+import notes from './notes'
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
-      <Note />
-      <Footer /> 
+      <div className='noteGrid'>
+        {notes.map(note => (
+          <Note
+            key={note.key}
+            note={note}
+          />
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,10 @@
 import '../Note.css'
 
-function Note() {
+function Note({note}) {
     return (
         <div className="Note">
-            <h3>Title</h3>
-            <p>This is a note.</p>
+            <h3>{note.title}</h3>
+            <p>{note.content}</p>
         </div>
     )
 }
